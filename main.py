a1fdb9d7e7e67a885898d2b0371ba518ed787e55
BazhenovVadim <Vadimbazhenov74@gmail.com>
2023-12-15 14:50:47 +0500

    develop vadim add dialog window, list parapetres, and some changes

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import sys
 
 # Импортируем из PyQt5.QtWidgets классы для создания приложения и виджета
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QMainWindow, QCheckBox
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QMainWindow, QCheckBox, QButtonGroup, \
+    QMessageBox
 from calorimeter_designer import Ui_MainWindow
-#from metods_to_calculate import harris_benedict_metod, mifflin_metod
+
+
+# from metods_to_calculate import harris_benedict_metod, mifflin_metod
 
 
 class Calorimeter(QMainWindow, Ui_MainWindow):
@@ -18,8 +21,17 @@ class Calorimeter(QMainWindow, Ui_MainWindow):
         self.gender = None
         self.purpose = None
         self.coof_activity = None
+        self.coof_by_purpose = None
         self.bmr_cal_mifflin = None
         self.bmr_cal_harris = None
+        self.tabWidget.setCurrentWidget(self.tab_daily_allowance)
+        self.groop_radiobutton_gender = QButtonGroup()
+        self.groop_radiobutton_gender.addButton(self.radioButton_man)
+        self.groop_radiobutton_gender.addButton(self.radioButton_woman)
+        self.groop_radiobutton_purpose = QButtonGroup()
+        self.groop_radiobutton_purpose.addButton(self.radioButton_minus_weight)
+        self.groop_radiobutton_purpose.addButton(self.radioButton_plus_weight)
+        self.groop_radiobutton_purpose.addButton(self.radioButton_save_weight)
 
     def connection_signals(self):
         self.radioButton_man.toggled.connect(self.check_gender)
@@ -28,13 +40,11 @@ class Calorimeter(QMainWindow, Ui_MainWindow):
         self.radioButton_minus_weight.toggled.connect(self.get_purpose)
         self.radioButton_save_weight.toggled.connect(self.get_purpose)
         self.pushButton_calculate_daily_allowance.clicked.connect(self.calculate_day_normal)
-        self.pushButton_clear_daily_allowance.clicked.connect(self.calculate_day_normal)
+        self.pushButton_clear_daily_allowance.clicked.connect(self.clear_settings_day_normal)
         self.comboBox_your_lifestyle.activated.connect(self.get_lifestyle)
         self.lineEdit_age.editingFinished.connect(self.get_age)
         self.lineEdit_height.editingFinished.connect(self.get_height)
         self.lineEdit_weight.editingFinished.connect(self.get_weight)
-
-
 
     def check_gender(self):
         if self.radioButton_man.isChecked():
@@ -62,8 +72,9 @@ class Calorimeter(QMainWindow, Ui_MainWindow):
                 "Легкая активность(упражнения 1-3 раза в неделю)": "1.55",
                 "Средняя активность(упражнения 3-5 раза в неделю)": "1.71",
                 "Высокая активность(высокие нагрузки каждый день)": "1.95",
-                "Экстримально-высокая нагрузка": "2.1"
+                "Экстремально-высокая нагрузка": "2.1"
             }
+
         self.coof_activity = float(self.dict_activity[self.lifestyle])
 
     def get_purpose(self):
@@ -74,38 +85,101 @@ class Calorimeter(QMainWindow, Ui_MainWindow):
         if self.radioButton_plus_weight.isChecked():
             self.purpose = self.radioButton_plus_weight.text()
 
-    def mifflin_metod(self):
-        if self.gender == "Мужской":
-            self.bmr_cal_mifflin = \
-                str(((10 * self.weiht) + (6.25 * self.height) - (5 * self.age) + 5) * self.coof_activity)
-            return self.bmr_cal_mifflin
-        if self.gender == "Женский":
-            self.bmr_cal_mifflin = \
-                str(((10 * self.weiht) + (6.25 * self.height) - (5 * self.age) - 161) * self.coof_activity)
-            return self.bmr_cal_mifflin
-
-    def harris_benedict_metod(self):
-        if self.gender == "Мужской":
-            self.bmr_cal_harris = \
-                str(((13.7516 * self.weiht) + (5.0033 * self.height) -
-                     (6.755 * self.age) + 66.473) * self.coof_activity)
-            return self.bmr_cal_harris
-        if self.gender == "Женский":
-            self. bmr_cal_harris = \
-                str(((9.5634 * self.weiht) + (1.8496 * self.height) -
-                     (4.6756 * self.age) + 655.0955) * self.coof_activity)
-            return self.bmr_cal_harris
+        self.dict_purpose_cal = \
+            {
+                "сбросить вес": -500,
+                "набрать вес": 300,
+                "сохранить вес": 0,
+            }
+        self.dict_for_list = \
+            {
+                "сбросить вес": "Рекомендации для потери веса :",
+                "набрать вес": "Рекомендации для набора веса :",
+                "сохранить вес": "Рекомендации для сохранения веса :",
+            }
+        self.dict_protein = \
+            {
+                "сбросить вес": 1.5,
+                "набрать вес": 2.5,
+                "сохранить вес": 1.1,
+            }
+        self.dict_yglevods = \
+            {
+                "сбросить вес": 1.5 ,
+                "набрать вес": 4,
+                "сохранить вес": 3,
+            }
+        self.dict_zhirs = \
+            {
+                "сбросить вес": 1,
+                "набрать вес": 2,
+                "сохранить вес": 1.5,
+            }
+        self.protein = self.weight * self.dict_protein[self.purpose]
+        self.yglevods = self.weight * self.dict_yglevods[self.purpose]
+        self.zhirs = self.weight * 0.5
+        self.recomend_text = self.dict_for_list[self.purpose]
+        self.coof_by_purpose = self.dict_purpose_cal[self.purpose]
 
     def calculate_day_normal(self):
         if self.pushButton_calculate_daily_allowance.clicked:
-            self.listWidget_daily_allowance.clear()
-            self.listWidget_daily_allowance.addItem("Рассчет суточной нормы калорий : \n")
-            self.listWidget_daily_allowance.addItem(f"По формуле Харриса-Бенедикта {self.bmr_cal_harris}")
-            self.listWidget_daily_allowance.addItem(f"По формуле Миффлина-Сан Жеора {self.bmr_cal_mifflin}")
-            print(self.lifestyle, self.age, self.coof_activity, self.weight, self.height, self.gender, self.purpose)
+            print(self.lifestyle, self.age, self.coof_activity, self.weight, self.height, self.gender, self.purpose,
+                  self.coof_by_purpose)
+            if (self.age is None or self.weight is None or self.height is None or self.lifestyle is None or
+                    self.gender is None or self.purpose is None or self.coof_activity is None or self.coof_by_purpose is None):
+                self.dialog = QMessageBox.critical(self, "Ошибка", "Заполните все поля")
+            else:
+                if self.gender == "Мужской":
+                    self.bmr_cal_mifflin = \
+                        str(int(((10 * self.weight) + (6.25 * self.height) - (5 * self.age) + 5) * self.coof_activity))
+                    self.bmr_cal_harris = \
+                        str(int(((13.7516 * self.weight) + (5.0033 * self.height) -
+                                 (6.755 * self.age) + 66.473) * self.coof_activity))
+                if self.gender == "Женский":
+                    self.bmr_cal_mifflin = \
+                        str(int(
+                            ((10 * self.weight) + (6.25 * self.height) - (5 * self.age) - 161) * self.coof_activity))
+                    self.bmr_cal_harris = \
+                        str(int(((9.5634 * self.weight) + (1.8496 * self.height) -
+                                 (4.6756 * self.age) + 655.0955) * self.coof_activity))
+                self.listWidget_daily_allowance.clear()
+                self.listWidget_daily_allowance.addItem("Рассчет суточной нормы калорий : \n")
+                self.listWidget_daily_allowance.addItem(f"По формуле Харриса-Бенедикта : \n {self.bmr_cal_harris} ккал")
+                self.listWidget_daily_allowance.addItem(
+                    f"По формуле Миффлина-Сан Жеора : \n {self.bmr_cal_mifflin} ккал \n")
+                self.listWidget_daily_allowance.addItem(self.recomend_text + "\n")
+                self.listWidget_daily_allowance.addItem("Суточная норма калорий" +
+                                                        str(int(self.bmr_cal_mifflin) - self.coof_by_purpose))
+                self.listWidget_daily_allowance.addItem(f"белков {self.protein}")
+                self.listWidget_daily_allowance.addItem(f"жиров {self.zhirs}")
+                self.listWidget_daily_allowance.addItem(f"углеводов {self.yglevods}")
 
-#TODO: решить проблему с self.bmr и его выводом на экран. Добавить очистку первого таба. Открывать прогу с 1 таба.
-#TODO: запретить пользователю вводить пустые поля(диалоговое окно). Добавить к расчету цель(похудеть, набрать)
+    def clear_settings_day_normal(self):
+        if self.pushButton_clear_daily_allowance.clicked:
+            self.groop_radiobutton_gender.setExclusive(False)
+            self.radioButton_man.setChecked(False)
+            self.radioButton_woman.setChecked(False)
+            self.groop_radiobutton_gender.setExclusive(True)
+            self.lineEdit_age.clear()
+            self.lineEdit_height.clear()
+            self.lineEdit_weight.clear()
+            self.comboBox_your_lifestyle.setCurrentIndex(0)
+            self.groop_radiobutton_purpose.setExclusive(False)
+            self.radioButton_minus_weight.setChecked(False)
+            self.radioButton_plus_weight.setChecked(False)
+            self.radioButton_save_weight.setChecked(False)
+            self.groop_radiobutton_purpose.setExclusive(True)
+            self.age = None
+            self.weight = None
+            self.height = None
+            self.lifestyle = None
+            self.gender = None
+            self.purpose = None
+            self.coof_activity = None
+            self.coof_by_purpose = None
+            self.bmr_cal_mifflin = None
+            self.bmr_cal_harris = None
+
 
 sys._excepthook = sys.excepthook
 
